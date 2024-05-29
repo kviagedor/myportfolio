2d1c57e20e81d709fe45b6125362aaacb235f448
Keneth Viagedor <keneth.viagedor@gmail.com>
2024-05-29 16:15:35 +0800

    updated themes

--- script.js
+++ script.js
@@ -94,3 +94,14 @@ form.addEventListener('submit', e => {
         console.error('Error!', error.message);
     });
 });
+
+var icon = document.getElementById("icon");
+
+icon.onclick = function(){
+    document.body.classList.toggle("dark-mode");
+    if(document.body.classList.contains("dark-mode")){
+        icon.src = "images/sun.png"
+    }else{
+        icon.src = "images/moon.png"
+    }
+}
